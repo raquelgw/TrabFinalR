d531f8a3364e6ae045149cd88ed68fe1d3ee8eb3
raquelgw <106815418+raquelgw@users.noreply.github.com>
2024-11-14 02:39:51 +0000

    ...

---
+++ src/database.ts
@@ -0,0 +1,26 @@
+import {open, Database} from "sqlite"
+import sqlite3 from 'sqlite3'
+
+let instance: Database | null = null
+
+export async function connect(){
+    if (instance != null)
+        return instance
+    
+    const db = await open({
+        filename: "database.sqlite",
+        driver: sqlite3.Database
+    })
+
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            email TEXT, 
+            senha TEXT
+        )    
+    `)
+    
+    instance = db
+    return db
+}
---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import express from "express"
+import { connect } from "./database"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static(__dirname + "/../public"))
+
+app.post("/usuario", async (req, res) => {
+    const db = await connect()
+    const { nome, email, senha } = req.body
+    const { lastID } = await db.run(`INSERT INTO usuarios(nome, email, senha) VALUES (?, ?, ?)`, [nome, email, senha])
+    res.json({ lastID })
+})
+
+app.post("/login", async (req, res) => {
+    const db = await connect()
+    const { email, senha } = req.body
+    const usuarios = await db.get("SELECT id, email FROM usuarios WHERE email = ? and senha = ?", [email, senha])
+    if (!usuarios){
+        res.status(401).json({mensagem:"Acesso negado"})
+        return
+    } 
+    res.json({token:"deu tudo certo "}) 
+})
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/public/login.html")
+// })
+
+// app.get("/cadastro", (req, res) => {
+//     res.sendFile(__dirname + "/public/cadastro.html")
+// })
+
+// let i = 0
+// app.get("/raquel", (req, res) => {
+//     console.log("oiiiiiiiiiiii")
+//     res.send("abc " + i++)
+// })
+
+// app.get("/quemsou/:pessoa", (req, res) => {
+//     res.send("vc é: " + req.params.pessoa)
+// })
+
+app.listen(3000, () => console.log("pronto"))
